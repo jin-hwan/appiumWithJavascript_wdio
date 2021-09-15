674d2a94ef49e8ef0efd9717caf0703361ebe873
jinhwan.choi <choi.jin.hwan@gmail.com>
2021-09-15 17:13:07 +0900

    isEnabled, isSelected, isDisplayed 스크립트 추가

--- test/pageobjects/dialog.page.js
+++ test/pageobjects/dialog.page.js
@@ -10,6 +10,16 @@ class Dialog {
     get dialogCancelBtn() { return $('//android.widget.Button[@resource-id="android:id/button2"]') }
 
     get repeatAlarmBtn() { return $('//android.widget.Button[@content-desc="Repeat alarm"]') }
+
+    get viewBtn() { return $('//android.widget.TextView[@content-desc="Views"]') }
+    get tabsBtn() { return $('//android.widget.TextView[@content-desc="Tabs"]') }
+    get contentByIdBtn() { return $('//android.widget.TextView[@content-desc="1. Content By Id"]') }
+
+    get tabs() { return $$('android.widget.LinearLayout') }
+    get tab1Details() { return $('~tab1') }
+    get tab2Details() { return $('~tab2') }
+    get tab3Details() { return $('~tab3') }
+
     _weekdayCheckbox(index) {
         return $(`//android.widget.CheckedTextView[@index="${index}"]`)
     }
--- test/specs/app/dialog.test.js
+++ test/specs/app/dialog.test.js
@@ -39,7 +39,7 @@ describe('Dialog', () => {
         driver.setTimeouts(1000)
         driver.pause(1000)
     })
-    it.only('Verify the repeat alarm options has attribute checked set to true when selected', () => {
+    it('Verify the repeat alarm options has attribute checked set to true when selected', () => {
         let isChecked, text;
         dialog.appBtn.click()
         dialog.alertDialogBtn.click()
@@ -56,6 +56,47 @@ describe('Dialog', () => {
         dialog._weekdayCheckbox(0).click()
         isChecked = dialog._weekdayCheckbox(0).getAttribute('checked')
         expect(isChecked).equal('true')
+
+    })
+    it.only('Verify isEnabled, isSelected, isDisplayed', () => {
+        dialog.viewBtn.click()
+        driver.touchAction([
+            { action: 'press', x: 500, y: 1400 },
+            { action: 'moveTo', x: 500, y: 300 },
+            'release',
+            { action: 'press', x: 500, y: 1400 },
+            { action: 'moveTo', x: 500, y: 300 },
+            'release',
+            { action: 'press', x: 500, y: 1400 },
+            { action: 'moveTo', x: 500, y: 300 },
+            'release'
+        ])
+
+        dialog.tabsBtn.click()
+        dialog.contentByIdBtn.click()
+
+        let isEnabled, isSelected, isDisplayed
+
+        // isEnabled = dialog.tabs.isEnabled() //$를 해야 selector를 찾음..
+        // isEnabled = dialog.tabs[0].isEnabled() //$$를 해야 selector를 찾음..
+        // console.log('isEnabled:', isEnabled)
+        for (i = 0; i < 3; i++) {
+            isEnabled = dialog.tabs[i].isEnabled()
+            isSelected = dialog.tabs[i].isSelected()
+            isDisplayed = dialog.tabs[i].isDisplayed()
+
+            console.log(`Tab ${i + 1}`)
+            console.log('isEnabled:', isEnabled)
+            console.log('isSelected:', isSelected)
+            console.log('isDisplayed:', isDisplayed)
+
+            if (isEnabled && isSelected) {
+                console.log('Tab Details 1: ', dialog.tab1Details.isDisplayed())
+                console.log('Tab Details 2: ', dialog.tab2Details.isDisplayed())
+                console.log('Tab Details 3: ', dialog.tab3Details.isDisplayed())
+            }
+
+        }
 
     })
 })

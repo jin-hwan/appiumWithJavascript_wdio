19a9a1d34a3bbfe4f67736e06dbd3f4cf8e5201d
jinhwan.choi <choi.jin.hwan@gmail.com>
2021-09-16 18:16:51 +0900

    POM적용

--- test/specs/web/login.js
+++ test/specs/web/login.js
@@ -3,15 +3,14 @@ const MainPage = require('../../pageobjects/google.page')
 
 describe('Login', () => {
     it.only('로그인 페이지 이동', async () => {
-        // await MainPage.open()
-        // await MainPage.searchTextbox.setValue('webdriverIO')
+        await MainPage.open()
+        await MainPage.searchTextbox.setValue('webdriverIO')
 
-        await browser.url('https://google.com')
+        // await browser.url('https://google.com')
+        // const searchTextbox = await $('input[name="q"')
 
-        const searchTextbox = await $('input[name="q"')
-        await browser.pause(3000)
-        await searchTextbox.click()
-        await searchTextbox.setValue('webdriverIO')
+        // await browser.pause(3000)
+        // await searchTextbox.setValue('webdriverIO')
         // await browser.back()
         await browser.keys('Enter') //PC에서 동작함을 확인
 
